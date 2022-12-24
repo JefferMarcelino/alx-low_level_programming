67e802943d55a2744eb6159cc555a418a52ada69
Jeffer Marcelino <jeffersunde72@gmail.com>
2022-12-24 12:42:06 +0200

    [0x04-more_functions_nested_loops - ADD]: Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+
+	for (n = 1; n < 101; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz");
+		else if (n % 3 == 0)
+			printf("Fizz");
+		else if (n % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", n);
+
+		if (n != 100)
+			printf(" ");
+	}
+
+	printf("\n");
+
+	return 0;
+}
