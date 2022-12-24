f75587abdc234c85b67591f3191b1da88688d9a3
Jeffer Marcelino <jeffersunde72@gmail.com>
2022-12-24 13:29:59 +0200

    [0x04-more_functions_nested_loops - FIX]: Task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,16 +10,15 @@
 
 void print_triangule(int size)
 {
-	int i, j, ch;
+	int i, j;
 
 	if (size > 0)
 	{
-		ch = 1;
 		for (i = 0; i < size; i++)
 		{
-			for (j = 0; j < size - ch; j++)
+			for (j = 0; j < size - i - 1; j++)
 				_putchar(32);
-			for (j = 0; j < ch; j++)
+			for (j = 0; j <= i; j++)
 				_putchar(35);
 			ch++;
 			_putchar(10);
