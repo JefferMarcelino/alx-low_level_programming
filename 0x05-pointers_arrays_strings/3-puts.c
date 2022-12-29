63207aee71d2fcda4d55ea22faa7a69d6d82de95
Jeffer Marcelino <jeffersunde72@gmail.com>
2022-12-29 17:37:51 +0200

    [0x05-pointers_arrays_strings - ADD]: Task 2

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string, with a new line
+ *
+ * @str: the string
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int n;
+
+	while (str[n] != 0)
+	{
+		_putchar(str[n]);
+	}
+	_putchar(10);
+}
