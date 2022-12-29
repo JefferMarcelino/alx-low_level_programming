0a52428a376451f5cb0151a89f5798b538d5000d
Jeffer Marcelino <jeffersunde72@gmail.com>
2022-12-29 18:03:48 +0200

    [0x05-pointers_arrays_strings - FIX]: Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -17,10 +17,7 @@ void print_rev(char *s)
 	while (s[size] != 0)
 		size++;
 
-	size--;
-	while (size >= 0)
-	{
+	size = size - 1;
+	for (size = size - 1; size > 0; size--)
 		_putchar(s[size]);
-		size--;
-	}
 }
