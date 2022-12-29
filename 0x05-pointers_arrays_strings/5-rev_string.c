448c117da7603b420b6ac5073f2e6a83b0ae3711
Jeffer Marcelino <jeffersunde72@gmail.com>
2022-12-29 18:57:22 +0200

    [0x05-pointers_arrays_strings - ADD]: Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverses a string
+ *
+ * @s: the string
+ *
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int pos = 0, size = 0;
+	char *str = *s;
+
+	while (s[size] != 0)
+		size++;
+
+	for (size = size - 1; size >= 0; --size, pos++)
+	{
+		*(s + pos) = str[size];
+	}
+}
