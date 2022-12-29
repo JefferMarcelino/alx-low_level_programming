0e7373c252903c4e5c84f26c2e7a4276bf65f7c5
Jeffer Marcelino <jeffersunde72@gmail.com>
2022-12-29 16:54:50 +0200

    [0x05-pointers_arrays_strings - ADD]: Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ *
+ * @s: the string
+ *
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int size = sizeof(s) / sizeof(*s[0]);
+
+	return (size);
+}
