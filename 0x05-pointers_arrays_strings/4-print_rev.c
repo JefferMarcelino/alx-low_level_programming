5baf499cc98d274906e57db2e643d786cc0915ec
Jeffer Marcelino <jeffersunde72@gmail.com>
2022-12-29 17:52:06 +0200

    [0x05-pointers_arrays_strings - ADD]: Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string a reverse order
+ *
+ * @s: the string to be printed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int size;
+
+	size = 0;
+
+	while (s[size] != 0)
+		size++;
+
+	while (size > 0)
+	{
+		_putchar(s[size]);
+		size--
+	}
+}
