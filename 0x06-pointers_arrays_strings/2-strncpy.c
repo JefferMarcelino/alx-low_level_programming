bd0cecb23bed5b06f49735e9b90dbd8f96e5270e
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-01-04 15:55:15 +0200

    [0x06-pointers_arrays_strings - FIX]: Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,16 @@
 #include "main.h"
 
+/**
+ * _strncpy - function that copies a string.
+ *            This function should work exactly like strncpy
+ *
+ * @dest: the first string
+ * @src: the second string
+ * @n: number of chars
+ *
+ * Return: the pointer to dest
+*/
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
