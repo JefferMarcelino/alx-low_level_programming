1fb6a1434f07ad3230ad41e4eace3bb7b29e6aef
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-01-04 17:32:56 +0200

    [0x06-pointers_arrays_strings - FIX]: Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,10 +14,15 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	char *ptr = dest;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i] != 0; i++)
 		dest[i] = src[i];
 
-	return (ptr);
+	while (i < n)
+	{
+		dest[i] = 0;
+		i++;
+	}
+
+	return dest;
 }
