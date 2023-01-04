b7eba19714c3e8245ccbb5f3c8bfcebbd26de14e
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-01-04 12:30:48 +0200

    [0x05-pointers_arrays_strings - ADD]: Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcpy - function that copies a string to a pointer
+ *
+ * @dest: the pointer that will contain the string
+ * @src: the string
+ *
+ * Return: the pointer to dest
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*src != 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
+	return (ptr);
+}
