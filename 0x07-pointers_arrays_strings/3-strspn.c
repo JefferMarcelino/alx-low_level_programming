3caabc297e0693418609aaaed0f967f9c801c18c
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-01-06 23:19:02 +0200

    [0x07-pointers_arrays_strings - FIX]: Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
+/**
+ * _strspn - function that gets the length of a prefix substring
+ *
+ * @s: the string that will be analyzed
+ * @accept: the character used to search
+ *
+ * Return: number of bytes in the initial segment of s which consist
+ *         only of bytes from accept
+*/
+
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
