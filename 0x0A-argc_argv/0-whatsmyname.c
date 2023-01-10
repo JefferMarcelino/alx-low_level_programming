8dd35e77ddff5a764c7fc9a180cd0f878a11bff0
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-01-10 17:41:51 +0200

    feat: task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program that prints its name
+ *
+ * @argc: arguments count
+ * @argv: arguments array
+ *
+ * Return: always 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
