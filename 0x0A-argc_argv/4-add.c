cd8f4ba9c0918351e6640e8be65a491e789e63ac
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-01-10 18:54:56 +0200

    fix(task 4): add isNum function to see if is a number

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,28 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * isNum - check if string array is num
+ * @num: string to check
+ * Return: 0 if it's a number
+ *         1 if it's not a number
+ */
+
+int isNum(char num[])
+{
+	int i, l = strlen(num);
+
+	for (i = 0; i < l; i++)
+	{
+		if (!isdigit(num[i]))
+			return (1);
+	}
+
+	return (0);
+}
 
 /**
  * main - program that adds positive numbers
@@ -17,7 +39,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (argv[i][0] >= 48 && argv[i][0] <= 57)
+		if (isNum(argv[i]) == 0)
 		{
 			sum += atoi(argv[i]);
 		} else
