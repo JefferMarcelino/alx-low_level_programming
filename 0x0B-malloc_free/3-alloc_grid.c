8a329c7b2d46edb6b3bb065bd5172b7e0b478333
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-01-15 22:57:20 +0200

    fix(0x0B-malloc_free - task 3): correcting the output

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	int i;
 	int **arr;
 
-	if (width < 0 || height < 0)
+	if (width <= 0 || height <= 0)
 		return (0);
 
 	arr = (int **)malloc(height * sizeof(int *));
