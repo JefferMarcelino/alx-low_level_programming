7564848bab1fcebf04d8b3e76ee4b6e3537ba24c
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-01-24 13:38:01 +0200

    fix(0x0C-more_malloc_free): initializing the memory with 0 on task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (0);
 
 	for (i = 0; i < (nmemb * size); i++)
-		*((char *)(array + i)) = 0;
+		*((char *)(array) + i) = 0;
 
 	return (array);
 }
