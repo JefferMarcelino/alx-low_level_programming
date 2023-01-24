5771cd5919c4a03b4fe4ca012d3719fd180c86b9
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-01-24 12:03:26 +0200

    feat(0x0C-more_malloc_free): adding task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+
+/**
+ * _strlen - function that count the chars from a string
+ *
+ * @str: the string
+ *
+ * Return: the string length
+ */
+
+int _strlen(char *str)
+{
+	if (*str == 0)
+		return (0);
+	str++;
+	return (1 + _strlen(str));
+}
+
+/**
+ * string_nconcat - function that concatenates two strings.
+ *
+ * @s1: the first string
+ * @s2: the second string
+ * @n: the number of bytes that should be consider on s2
+ *
+ * Return: the new string
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *newStr;
+	int i, totalSize;
+
+	if (n >= _strlen(s2))
+	{
+		totalSize = _strlen(s1) + _strlen(s2);
+	}
+	else
+	{
+		totalSize = _strlen(s1) + n;
+	}
+
+	newStr = malloc(totalSize + 1);
+
+	if (newStr == 0)
+		return (0);
+
+	for (i = 0; i < _strlen(s1); i++)
+	{
+		newStr[i] = s1[i];
+	}
+
+	for (i = 0; i < n && s2[i] != 0; i++)
+	{
+		newStr[_strlen(s1) + i] = s2[i];
+	}
+
+	newStr[totalSize] = 0;
+
+	return (newStr);
+}
