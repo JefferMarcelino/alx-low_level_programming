266dc4454386d2bd218025f74cff337f34fa8c28
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-01-26 19:57:15 +0200

    feat(0x0E-structures_typedef - 1.init_dog.c): creating the function that initiliaze the dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "dog.h"
+
+/**
+ * init_dog - function that gives a name, age and owner to a dog
+ *
+ * @d: the pointer to a dog
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
