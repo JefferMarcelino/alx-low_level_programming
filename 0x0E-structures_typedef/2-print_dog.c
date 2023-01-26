8e6493c54bc54067b317f498ddb59a0c02dc7745
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-01-26 20:38:47 +0200

    fix(0x0E-structures_typedef - 2-print_dog.c): print (nil) in case of null value

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,12 +14,8 @@ void print_dog(struct dog *d)
 {
 	if (d != 0)
 	{
-		printf("Name: %s\n", (d->name ? d->name : "(nil)"));
-
-		if (d->age > 0)
-			printf("Age: %f\n", d->age);
-		else
-			printf("Age: %s\n", "(nil)");
-		printf("Owner: %s\n", (d->owner ? d->owner : "nil"));
+		printf("Name: %s\n", (d->name != 0 ? d->name : "(nil)"));
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", (d->owner != 0 ? d->owner : "nil"));
 	}
 }
