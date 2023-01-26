497d404d38b0614e322703262daab335131aaf68
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-01-26 19:35:46 +0200

    feat(0x0E-structures_typedef - dog.h): creating the dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_
+
+#define _DOG_
+/**
+ * struct dog - a struct that represents a dog
+ *
+ * @name: the name of the dog
+ * @owner: the owner of the dog
+ * @age: the dog's age
+ */
+
+typedef struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+} dog;
+
+#endif
