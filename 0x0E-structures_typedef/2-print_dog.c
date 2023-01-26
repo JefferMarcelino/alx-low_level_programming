137b20cd64e1d15995e6a6dc49fed7c37e986a53
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-01-26 20:25:25 +0200

    fix(0x0E-structures_typedef - 2-print_dog.c): print (nil) in case of invalid age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,7 +16,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", (d->name ? d->name : "(nil)"));
 
-		if (d->age != 0)
+		if (d->age > 0)
 			printf("Age: %f\n", d->age);
 		else
 			printf("Age: %s\n", "(nil)");
