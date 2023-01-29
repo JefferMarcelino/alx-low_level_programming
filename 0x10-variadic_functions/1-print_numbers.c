ad4fd75288347a963e1f4510550dd06f9be9c072
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-01-30 00:31:30 +0200

    fix(0x10-variadic_functions - task 1): printing a new line at the end

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,8 +19,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		if (i == (n - 1))
-			printf("%d\n", va_arg(args, int));
+			printf("%d", va_arg(args, int));
 		else
 			printf("%d%s", va_arg(args, int), (separator ? separator : ""));
 	}
+
+	printf("\n");
 }
