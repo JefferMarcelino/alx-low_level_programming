4e7f6bd7b4e771680aec62414a048ff3f1940f3d
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-02-05 13:31:08 +0200

    fix(0x14-bit_manipulation - task 4): betty code style

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,6 +1,15 @@
 #include "main.h"
 #define UN_L_INT_SIZE ((sizeof(unsigned long int) * (8)))
 
+/**
+ * clear_bit - function that sets the value of a bit to 0 at a given index
+ *
+ * @n: the value
+ * @index: the index
+ *
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask = 1;
