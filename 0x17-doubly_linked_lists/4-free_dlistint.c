3f7c84f451ecc972e210ba92289336f5586921e5
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-04-01 21:25:41 +0200

    fix(0x17-doubly_linked_lists - task 4): betty code style

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,6 +1,14 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * free_dlistint - a function that frees a dlistint_t list
+ *
+ * @head: the head of the linked list
+ *
+ * Return: nothing
+ */
+
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *current;
