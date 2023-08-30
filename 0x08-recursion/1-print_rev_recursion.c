7715494612ab4a63cb4d91a7a9f9c94b21394197
Jeffer Marcelino <jeffersunde72@gmail.com>
2023-08-30 18:01:37 +0200

    fix(task 1)

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,8 +10,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	if ((s != 0 || *s != 0) && *(s++) != 0)
+	if (*s != 0)
+	{
+		s++;
 		_print_rev_recursion(s);
-
-	_putchar(*(--s));
+		_putchar(*(--s));
+	}
 }
